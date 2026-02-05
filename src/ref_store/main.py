08ed66405462ececd2f36e58003386db076ce52b
Ben Stabley <ben.stabley@nv5.com>
2026-02-05 12:24:38 -0800

    not absolute path

--- src/ref_store/main.py
+++ src/ref_store/main.py
@@ -3,7 +3,7 @@ from ref_store import manifest
 
 
 def main():
-    root = Path("C:/work/ref_store/src")
+    root = Path("./src")
     print(manifest.for_directory(root, "https://github.com/bstableynv5/ref_store"))
 
 
